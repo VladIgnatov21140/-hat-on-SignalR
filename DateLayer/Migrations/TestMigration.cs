17dd8bdd301131796d5dcdaf3c0599c535845e52
VladIgnatov21140 <vlad.ignatov.2001@gmail.com>
2019-11-22 22:35:53 +0300

    Изменена структура размещения папок проекта и библиотек классов.

---
+++ DateLayer/Migrations/TestMigration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentMigrator;
+
+namespace DateLayer.Migrations
+{
+    /// <summary>
+    /// First migration for database
+    /// </summary>
+    [Migration(019112019)]
+    internal class TestMigration : Migration
+    {
+        // TODO: Write migration
+        public override void Up()
+        { 
+            
+        }        
+        
+        public override void Down()
+        {
+            
+        }
+
+    }
+}
